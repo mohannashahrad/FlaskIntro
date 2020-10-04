5931280e9aa9f77fe23fc6a6ba5734d60c022640
Janine <janine.newton@mail.utoronto.ca>
2020-10-04 14:24:03 -0400

    removed old frontend files

--- frontend/old/src/components/pages/Calendar.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Calendar = () => {
-    return (
-        <h2>Calendar </h2>
-    )
-}
--- frontend/old/src/components/pages/Notes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Notes = () => {
-    return (
-        <h2>
-            Notes
-        </h2>
-    )
-}
